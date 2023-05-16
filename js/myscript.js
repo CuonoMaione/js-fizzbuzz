7c77f1f2453f25400a232fe02b676e44cfc0b506
CuonoMaione <cuonomaione99@outlook.it>
2023-05-16 16:30:25 +0200

    js-fizzbuzz

---
+++ js/myscript.js
@@ -0,0 +1,40 @@
+
+const UlElement = document.querySelector("ul");
+
+for (let i = 1 ; i < 101 ; i++) {
+
+    const liElement = document.createElement("li");
+   
+    liElement.append(i)
+
+    UlElement.append(liElement);
+
+    liElement.classList.add("normal")
+
+    if ( i % 3 === 0) {
+        liElement.classList.remove("normal")
+        liElement.classList.add("red");
+        liElement.innerHTML = "fizz"
+    } ;
+
+    if ( i % 5 === 0 ){
+        liElement.classList.remove("normal")
+        liElement.classList.add("lime")
+        liElement.innerHTML = "buzz"
+    } ; 
+
+    if ( i % 5 === 0 && i % 3 === 0){
+        liElement.classList.remove("normal")
+        liElement.classList.add("yellow")
+        liElement.innerHTML = "fizzbuzz"
+    } ;
+}
+
+
+
+
+
+
+
+
+
